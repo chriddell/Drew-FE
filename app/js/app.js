8300b1b0c3a304f466302c16509860ec44f07fb1
Chris Liddell <chrisliddell1991@gmail.com>
2017-09-20 10:56:44 +0100

    Fix nav issues. Prevent scroll on mobile nav visible.

--- app/js/app.js
+++ app/js/app.js
@@ -20,13 +20,15 @@
 (function(){
 
 	// menu vars
-	var menuTrigger = document.querySelector( '#toggle-nav-menu' ),
-			menu 				= document.querySelector( '#nav-menu' ),
-			activeClass	= 'is-active';
+	var menuTrigger		= document.querySelector( '#toggle-nav-menu' ),
+			menu 					= document.querySelector( '#nav-menu' ),
+			activeClass		= 'is-active',
+			noScrollClass	= 'js-no-scroll';
 
 	function toggleMenu() {
 		menu.classList.toggle(activeClass);
 		menuTrigger.classList.toggle(activeClass);
+		document.body.classList.toggle(noScrollClass);
 	}
 
 	menuTrigger.addEventListener( 'click', toggleMenu );
